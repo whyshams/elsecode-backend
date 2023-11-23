01c940f477050c3f2367bc3d5954d0b2310ed579
Nuren Shams Chowdhury <chowdhury.noren@gmail.com>
2023-11-24 00:49:19 +0600

    abar

--- server.js
+++ server.js
@@ -9,7 +9,5 @@ const app = express();
 app.use(cors());
 app.use(urlencoded({ extended: true }));
 app.use(express.json());
-app.listen(5000, () => {
-  console.log("listening");
-});
+
 app.use("/contact", router);
